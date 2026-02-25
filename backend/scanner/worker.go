54ac38bd39f93fc74934eb40c30a0ce0ffb0098a
Arnel-Rah <arnel.raharinandrasana@student.passerellesnumeriques.org>
2026-02-25 18:17:24 +0300

    feat(scanner): trigger initial scan on startup and log latency

--- backend/scanner/worker.go
+++ backend/scanner/worker.go
@@ -9,24 +9,30 @@ import (
 
 func StartWorker(db *gorm.DB) {
 	ticker := time.NewTicker(1 * time.Minute)
+	performScan := func() {
+		var sites []models.Site
+		if err := db.Find(&sites).Error; err != nil {
+			return
+		}
+
+		for _, site := range sites {
+			go func(s models.Site) {
+				result := CheckSite(s.URL, 10*time.Second)
+				
+				db.Model(&s).Updates(map[string]interface{}{
+					"is_up":       result.Up,
+					"last_status": 200, 
+					"latency":     result.Latency.Milliseconds(),
+					"updated_at":  time.Now(),
+				})
+			}(site)
+		}
+	}
 
 	go func() {
+		performScan()
 		for range ticker.C {
-			var sites []models.Site
-			if err := db.Find(&sites).Error; err != nil {
-				continue
-			}
-
-			for _, site := range sites {
-				go func(s models.Site) {
-					result := CheckSite(s.URL, 10*time.Second)
-						db.Model(&s).Updates(map[string]interface{}{
-						"is_up":       result.Up,
-						"last_status": 200, 
-						"updated_at":  time.Now(),
-					})
-				}(site)
-			}
+			performScan()
 		}
 	}()
 }
