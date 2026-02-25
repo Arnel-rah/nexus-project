a5c5bb54412dfef98403688d08876c6fc181237b
Arnel-Rah <arnel.raharinandrasana@student.passerellesnumeriques.org>
2026-02-25 19:55:45 +0300

    feat(models): add SiteHistory model and History relation to Site

--- backend/models/site.go
+++ backend/models/site.go
@@ -7,10 +7,19 @@ import (
 
 type Site struct {
     gorm.Model
-    Name       string    `json:"name"`
-    URL        string    `json:"url" gorm:"unique;not null"`
-    Latency    int64     `json:"latency"` 
-    IsUp       bool      `json:"is_up"`
-    LastStatus int       `json:"last_status"`
-    LastCheck  time.Time `json:"last_check"`
+    Name       string        `json:"name"`
+    URL        string        `json:"url" gorm:"unique;not null"`
+    Latency    int64         `json:"latency"`
+    IsUp       bool          `json:"is_up"`
+    LastStatus int           `json:"last_status"`
+    LastCheck  time.Time     `json:"last_check"`
+    History    []SiteHistory `json:"history" gorm:"foreignKey:SiteID"`
+}
+
+type SiteHistory struct {
+    gorm.Model
+    SiteID    uint      `json:"site_id" gorm:"index"`
+    IsUp      bool      `json:"is_up"`
+    Latency   int64     `json:"latency"`
+    CheckedAt time.Time `json:"checked_at"`
 }
